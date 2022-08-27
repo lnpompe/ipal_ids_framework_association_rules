13ec9404e3e8290db3aea3c6102b24ec9b42fb2f
Leonardo Pompe <leonardo.pompe@rwth-aachen.de>
2022-08-27 18:37:36 +0200

    ids: None packets are now considered benign

--- eval_live.py
+++ eval_live.py
@@ -3,6 +3,8 @@ import sys
 
 # expects eval_file to point to a file which is the output from the live phase on non-malicious data
 def eval_fpr(eval_file):
+    print(f"Computing False Positive Rate for file {eval_file}")
+
     # false positives
     fp = 0
     # true negatives
@@ -11,12 +13,11 @@ def eval_fpr(eval_file):
     with open(eval_file, "r") as input_file:
         for line in input_file.readlines():
             packet = json.loads(line)
-            if packet["ids"]:
-                fp = fp + 1
-            elif not packet["ids"]:
-                tn = tn + 1
-            else:
-                print(f"Warning! Packet with timestamp {packet['timestamp']} does not have an IDS value assigned.")
+            if packet["ids"] is not None:
+                if packet["ids"]:
+                    fp = fp + 1
+                elif not packet["ids"]:
+                    tn = tn + 1
     print(f"False Positives: {fp}")
     print(f"True Negatives: {tn}")
     print(f"False Positive Rate: {fp/(fp+tn)}")
