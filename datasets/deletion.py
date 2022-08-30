6769404e39b354de018da9472e004667e64681aa
Stefan Lenz <stefan.lenz@rwth-aachen.de>
2022-08-30 13:06:40 +0200

    update eval/plot scripts

--- datasets/deletion.py
+++ datasets/deletion.py
@@ -7,9 +7,10 @@ diff_file = './normal_30_02_removed_diff.json'
 # packet removal
 #
 df = pd.read_json(in_file, lines=True)
+df['timestamp'] = df['timestamp'].astype('float')
 df.reset_index(drop=True)
 df.set_index('id')
-print(df['timestamp'])
+print(type(df['timestamp'][1]))
 sample = df.sample(frac=0.9)
 sample = sample.sort_index()
 print(sample['timestamp'])
@@ -17,8 +18,10 @@ print(sample['timestamp'])
 diff = pd.concat([df, sample]).drop_duplicates(subset=['id'], keep=False)
 diff.reset_index(drop=True)
 diff.set_index('id')
+print(diff['timestamp'])
+
 diff["start"] = diff["timestamp"]
 diff["end"] = diff["timestamp"]
 
 sample.to_json(out_file, orient='records', lines=True)
-diff.to_json(diff_file, orient='records', indent=2)
+diff.to_json(diff_file, orient='records', line=True)
