8814b2695c78c0e17f7de973a814a765ecbb5d37
Stefan Lenz <stefan.lenz@rwth-aachen.de>
2022-08-30 10:43:35 +0200

    update eval/plot scripts

--- datasets/deletion.py
+++ datasets/deletion.py
@@ -9,14 +9,16 @@ diff_file = './normal_30_02_removed_diff.json'
 df = pd.read_json(in_file, lines=True)
 df.reset_index(drop=True)
 df.set_index('id')
-sample = df.sample(frac=0.9, random_state=690537191)
+print(df['timestamp'])
+sample = df.sample(frac=0.9)
+sample = sample.sort_index()
+print(sample['timestamp'])
+
 diff = pd.concat([df, sample]).drop_duplicates(subset=['id'], keep=False)
 diff.reset_index(drop=True)
 diff.set_index('id')
 diff["start"] = diff["timestamp"]
 diff["end"] = diff["timestamp"]
 
-
 sample.to_json(out_file, orient='records', lines=True)
 diff.to_json(diff_file, orient='records', indent=2)
-
