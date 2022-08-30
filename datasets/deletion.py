e87585fdb7f45e5b8f5d1b53302696055780bd69
Leonardo Pompe <leonardo.pompe@rwth-aachen.de>
2022-08-30 12:40:06 +0200

    add scripts for testing detection of deleted packets

--- datasets/deletion.py
+++ datasets/deletion.py
@@ -1,22 +1,24 @@
 import pandas as pd
+import sys
 import numpy as np
+PLACEHOLDER = "placeholder123"
 
-in_file = 'normal_30_02.ipal'
-out_file = './normal_30_02_removed.ipal'
-diff_file = './normal_30_02_removed_diff.json'
+in_file = sys.argv[1] #'GRFICS/30min/normal_30min_01_copy2.ipal'
+out_file = sys.argv[2] #'GRFICS/attacks/normal_30_01_removed.ipal'
+diff_file = sys.argv[3] #'GRFICS/attacks/normal_30_01_removed_diff.json'
 # packet removal
 #
 df = pd.read_json(in_file, lines=True)
 df.reset_index(drop=True)
 df.set_index('id')
-sample = df.sample(frac=0.9, random_state=690537191)
+sample = df.sample(frac=0.9, random_state=690537191).sort_index()
 diff = pd.concat([df, sample]).drop_duplicates(subset=['id'], keep=False)
 diff.reset_index(drop=True)
 diff.set_index('id')
-diff["start"] = diff["timestamp"]
-diff["end"] = diff["timestamp"]
+diff["start"] = diff[PLACEHOLDER]
+diff["end"] = diff[PLACEHOLDER]
 
 
+sample[PLACEHOLDER] = sample[PLACEHOLDER].astype(float)
 sample.to_json(out_file, orient='records', lines=True)
 diff.to_json(diff_file, orient='records', indent=2)
-
---
+++ datasets/deletion.sh
@@ -0,0 +1,5 @@
+sed 's/timestamp/placeholder123/g' $1 > temp1.ipal
+python datasets/deletion.py temp1.ipal temp2.ipal temp3.json
+sed 's/placeholder123/timestamp/g' temp2.ipal > $2
+sed 's/placeholder123/timestamp/g' temp3.json > $3
+rm temp1.ipal temp2.ipal temp3.json
